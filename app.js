9287c0c242fe366865ce438081358ea40cddd40e
Jason Donley <jay.donley@gmail.com>
2022-10-12 19:09:38 -0400

    funWithVariablesHomework

---
+++ app.js
@@ -0,0 +1,33 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+
+console.log(a + b);
+console.log(c + d);
+
+console.log(f - e);
+console.log(h - g);
+
+console.log(i * j);
+console.log(a * b);
+
+console.log(j / b);
+console.log(h / d);
+
+
+++a;
+
+++b;
+
+--c;
+
+--d;
+
